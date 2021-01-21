40a1d3879fb52c278fd106fb1c70b81165a79db7
최광석 <compert@svcorps.dev>
2021-01-21 14:47:16 +0900

    필터 스크린 작업 중

--- src/screens/HomeScreens/FilterScreen/index.js
+++ src/screens/HomeScreens/FilterScreen/index.js
@@ -4,48 +4,140 @@ import {connect} from 'react-redux'
 import FastImage from 'react-native-fast-image'
 import _ from 'lodash'
 
-import CodePush from '../../common/CodePush'
 import mConst from '../../../common/constants'
+import mUtils from '../../../common/utils'
 import cBind, {callOnce} from '../../../common/navigation'
 import Text from '../../common/Text'
-// import {Col, Row, Grid} from '../../common/EasyGrid'
-import {Col, Row, Grid} from 'react-native-easy-grid'
+import {Grid, Col, Row} from 'react-native-easy-grid'
 import styles from './styles'
+
+const genders = ['WOMEN', 'MEN', 'Unisex']
+const sections = ['Brands', 'Category', 'Availability', 'Color', 'Size', 'Sample', 'Still Life Image', 'Material']
 
 class FilterScreen extends PureComponent {
   constructor(props) {
     super(props)
     cBind(this)
+    this.state = {
+      gender: genders[0],
+      section: sections[0],
+    }
   }
   componentDidMount() {
     this.modalOption('FILTER')
   }
+  handleSelectGender = gender => {
+    this.setState({gender})
+  }
+  handleSelectSection = section => {
+    this.setState({section})
+  }
   render() {
+    const {gender, section} = this.state
     return (
-      // <SafeAreaView style={styles.container}>
-      //   {/* <Grid>
-      //     <Row>
-      //       <Col style={{background: 'red'}}></Col>
-      //       <Col style={{background: 'green'}}></Col>
-      //     </Row>
-      //   </Grid> */}
-      //   <Text>11</Text>
-      //   <Grid style={{flex: 1, height: 1000}}>
-      //     <Row style={{flex: 1, backgroundColor: 'red', height: 100}} size={10}></Row>
-      //     <Row style={{flex: 1, backgroundColor: 'green', height: 100}} size={10}></Row>
-      //   </Grid>
-      // </SafeAreaView>
       <SafeAreaView style={styles.container}>
         <Grid>
-          <Col style={{backgroundColor: 'red'}}>
-            <Text>1</Text>
-          </Col>
-          <Col>
-            {/* <Row style={{backgroundColor: 'yellow'}}><Text>2</Text></Row>
-            <Row style={{backgroundColor: 'green'}}>
-              <Text>3</Text>
-            </Row> */}
-          </Col>
+          <Row style={styles.headerWrapper}>
+            {_.map(genders, (item, index) => {
+              const selected = item === gender
+              return (
+                <TouchableOpacity key={index} onPress={() => this.handleSelectGender(item)}>
+                  <Text style={selected ? styles.headerTextOn : styles.headerText}>{item}</Text>
+                </TouchableOpacity>
+              )
+            })}
+          </Row>
+          <Row>
+            <Col size={35}>
+              {_.map(sections, (item, index) => {
+                const selected = item === section
+                return (
+                  <TouchableOpacity onPress={() => this.handleSelectSection(item)}>
+                    <Row style={selected ? styles.sectionWrapperOn : styles.sectionWrapper}>
+                      <Text style={selected ? styles.sectionTextOn : styles.sectionText}>{item}</Text>
+                    </Row>
+                  </TouchableOpacity>
+                )
+              })}
+            </Col>
+            <Col size={65}>
+              {/* <TouchableOpacity onPress={() => this.alert('', 'ok2')}> */}
+              <Row
+                style={{
+                  // width: mConst.wWidth,
+                  height: mUtils.wScale(50),
+                  alignItems: 'center',
+                  paddingHorizontal: mUtils.wScale(5),
+                  borderTopWidth: 1,
+                  borderColor: 'black',
+                  backgroundColor: 'yellow',
+                }}
+              >
+                <Text>Category (2)</Text>
+              </Row>
+              {/* </TouchableOpacity>
+              <TouchableOpacity onPress={() => this.alert('', 'ok2')}> */}
+              <Row
+                style={{
+                  // width: mConst.wWidth,
+                  height: mUtils.wScale(50),
+                  alignItems: 'center',
+                  paddingHorizontal: mUtils.wScale(5),
+                  borderTopWidth: 1,
+                  borderColor: 'black',
+                  backgroundColor: 'yellow',
+                }}
+              >
+                <Text>Category (2)</Text>
+              </Row>
+              {/* </TouchableOpacity>
+              <TouchableOpacity onPress={() => this.alert('', 'ok2')}> */}
+              <Row
+                style={{
+                  // width: mConst.wWidth,
+                  height: mUtils.wScale(50),
+                  alignItems: 'center',
+                  paddingHorizontal: mUtils.wScale(5),
+                  borderTopWidth: 1,
+                  borderColor: 'black',
+                  backgroundColor: 'yellow',
+                }}
+              >
+                <Text>Category (2)</Text>
+              </Row>
+              {/* </TouchableOpacity>
+              <TouchableOpacity onPress={() => this.alert('', 'ok2')}> */}
+              <Row
+                style={{
+                  // width: mConst.wWidth,
+                  height: mUtils.wScale(50),
+                  alignItems: 'center',
+                  paddingHorizontal: mUtils.wScale(5),
+                  borderTopWidth: 1,
+                  borderColor: 'black',
+                  backgroundColor: 'yellow',
+                }}
+              >
+                <Text>Category (2)</Text>
+              </Row>
+              {/* </TouchableOpacity>
+              <TouchableOpacity onPress={() => this.alert('', 'ok2')}> */}
+              <Row
+                style={{
+                  // width: mConst.wWidth,
+                  height: mUtils.wScale(50),
+                  alignItems: 'center',
+                  paddingHorizontal: mUtils.wScale(5),
+                  borderTopWidth: 1,
+                  borderColor: 'black',
+                  backgroundColor: 'yellow',
+                }}
+              >
+                <Text>Category (2)</Text>
+              </Row>
+              {/* </TouchableOpacity> */}
+            </Col>
+          </Row>
         </Grid>
         <Grid>
           <Row style={{width: '30%', backgroundColor: 'yellow'}}>
--- src/screens/HomeScreens/FilterScreen/styles.js
+++ src/screens/HomeScreens/FilterScreen/styles.js
@@ -10,4 +10,46 @@ export default StyleSheet.create({
     alignItems: 'center',
     backgroundColor: mConst.white,
   },
+  headerWrapper: {
+    width: mConst.wWidth,
+    height: mUtils.wScale(60),
+    alignItems: 'center',
+    paddingHorizontal: mUtils.wScale(5),
+    borderTopWidth: 1,
+    borderColor: mConst.black,
+  },
+  headerText: {
+    padding: mUtils.wScale(15),
+    fontSize: 15,
+    color: mConst.gray,
+  },
+  headerTextOn: {
+    padding: mUtils.wScale(15),
+    fontSize: 15,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+  },
+  sectionWrapper: {
+    height: mUtils.wScale(60),
+    alignItems: 'center',
+    paddingHorizontal: mUtils.wScale(20),
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderRightWidth: StyleSheet.hairlineWidth,
+    borderColor: mConst.borderGray,
+  },
+  sectionWrapperOn: {
+    height: mUtils.wScale(60),
+    alignItems: 'center',
+    paddingHorizontal: mUtils.wScale(20),
+    backgroundColor: mConst.black,
+  },
+  sectionText: {
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  sectionTextOn: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: mConst.white,
+  },
 })
