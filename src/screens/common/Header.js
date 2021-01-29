61f4fe1be597010cbe8ab48ac01938491a6d0e70
최광석 <compert@svcorps.dev>
2021-01-29 11:14:41 +0900

    화면 공통 헤더 패딩 보정(안드로이드 대응)

--- src/screens/common/Header.js
+++ src/screens/common/Header.js
@@ -58,6 +58,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignSelf: 'center',
     width: mConst.wWidth - mUtils.wScale(40),
+    marginVertical: mConst.bAndroid ? mUtils.wScale(10) : mUtils.wScale(5),
   },
   inputTextWrapper: {
     width: mConst.wWidth - mConst.wGapUnit * 2.8,
@@ -73,6 +74,7 @@ const styles = StyleSheet.create({
   input: {
     width: mConst.wWidth - mConst.wGapUnit * 4,
     fontSize: mUtils.wScale(15),
+    padding: 0,
     color: mConst.textBaseColor,
   },
   inputIconWrapper: {
