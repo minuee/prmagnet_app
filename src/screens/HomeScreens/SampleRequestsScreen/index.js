eeae0563efb4377ea7e3f8b97ae10921f52928b0
LeeKyungJoon <wns965@svcorps.dev>
2021-02-05 18:29:25 +0900

    ***SampleRequests 화면 및 로직 추가

--- src/screens/HomeScreens/SampleRequestsScreen/index.js
+++ src/screens/HomeScreens/SampleRequestsScreen/index.js
@@ -3,7 +3,7 @@ import {SafeAreaView, View, ScrollView, FlatList, TouchableOpacity, Pressable} f
 import {connect} from 'react-redux'
 import FastImage from 'react-native-fast-image'
 import _ from 'lodash'
-import Header from '../../common/Header'
+import Header1 from '../../common/Header1'
 import Modal from 'react-native-modal'
 
 import mConst from '../../../common/constants'
@@ -13,37 +13,239 @@ import Text from '../../common/Text'
 import {Grid, Col, Row} from 'react-native-easy-grid'
 import styles from './styles'
 import {multicastChannel} from 'redux-saga'
+import {TextInput} from 'react-native'
 
 const modelImg = require('../../../images/navi/model_1.png')
-const newImg = require('../../../images/navi/new_1.png')
-const notiImg = require('../../../images/navi/noti_1.png')
-const telImg = require('../../../images/navi/tel_1.png')
-const fixImg = require('../../../images/navi/fix_1.png')
-const settingImg = require('../../../images/navi/setting_1.png')
-const bookImg = require('../../../images/navi/book_1.png')
-const moreImg = require('../../../images/navi/more_4.png')
-const crownImg = require('../../../images/navi/crown_1.png')
-const selectImg1 = require('../../../images/navi/select_1.png')
-const selectImg2 = require('../../../images/navi/select_2.png')
+const moreImg = require('../../../images/navi/more_2.png')
+const starImg = require('../../../images/navi/star_1.png')
+const checkImg = require('../../../images/navi/check_1.png')
+const noCheckImg = require('../../../images/navi/no_check_1.png')
+const plusImg = require('../../../images/navi/plus_2.png')
+const checkImg2 = require('../../../images/navi/check_2.png')
+const checkImg3 = require('../../../images/navi/check_3.png')
+const yesNo = ['Yes', 'No']
 
 class SampleRequestsScreen extends PureComponent {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      data: [
+        {img: modelImg, title: 'Look #1'},
+        {img: modelImg, title: 'Look #2'},
+        {img: modelImg, title: 'Look #3'},
+        {img: modelImg, title: 'Look #4'},
+      ],
+      selected: [],
+      yesNo: '',
+    }
+  }
+
+  select = model => {
+    this.setState()
   }
 
   render() {
+    const {data} = this.state
     return (
       <SafeAreaView style={styles.container}>
-        <Header />
-        <ScrollView style={{paddingHorizontal: mUtils.wScale(20)}}>
-          <Text style={{...styles.mainTitle, marginTop: mUtils.wScale(25)}}>Sample</Text>
-          <Text style={styles.mainTitle1}>Requests</Text>
-          <Text style={{...styles.subTitle, marginTop: mUtils.wScale(30)}}>
-            Request product : <Text style={{color: '#7ea1b2'}}>4</Text>
-          </Text>
-          <ScrollView horizontal={true} style={{backgroundColor: 'red'}}></ScrollView>
+        <Header1 />
+        <ScrollView>
+          <View style={{paddingHorizontal: mUtils.wScale(20)}}>
+            <Text style={{...styles.mainTitle, marginTop: mUtils.wScale(25)}}>Sample</Text>
+            <Text style={styles.mainTitle1}>Requests</Text>
+            <Text style={{...styles.subTitle, marginTop: mUtils.wScale(30)}}>
+              Request product : <Text style={{color: '#7ea1b2'}}>4</Text>
+            </Text>
+          </View>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{marginLeft: mUtils.wScale(20), marginTop: mUtils.wScale(16)}}>
+            {data.map((item, index) => {
+              return (
+                <View key={index} style={{marginRight: mUtils.wScale(5), alignItems: 'center'}}>
+                  <TouchableOpacity>
+                    <FastImage resizeMode={'contain'} style={styles.modelImg} source={item.img} />
+                  </TouchableOpacity>
+                  <Text style={{...styles.modelTitle, marginTop: mUtils.wScale(8)}}>{item.title}</Text>
+                </View>
+              )
+            })}
+          </ScrollView>
+          <View style={styles.emptyBar} />
+          <View style={{paddingHorizontal: mUtils.wScale(20)}}>
+            <Text style={{...styles.subTitle}}>Request Information</Text>
+            <View
+              style={{
+                ...styles.layout,
+                justifyContent: 'space-between',
+                paddingTop: mUtils.wScale(20),
+                paddingBottom: mUtils.wScale(18),
+              }}
+            >
+              <View style={{width: '49%'}}>
+                <Text style={styles.smallTitle}>Magazine</Text>
+                <TouchableOpacity style={{...styles.box1, justifyContent: 'space-between'}}>
+                  <Text style={styles.boxText}>RTW</Text>
+                  <FastImage resizeMode={'contain'} style={styles.moreImg} source={moreImg} />
+                </TouchableOpacity>
+              </View>
+              <View style={{width: '49%'}}>
+                <Text style={styles.smallTitle}>Editor/Stylist</Text>
+                <TextInput style={{...styles.inputBox}} placeholder={'이름'} placeholderTextColor={mConst.borderGray} />
+              </View>
+            </View>
+            <Text style={styles.smallTitle}>
+              Contact <FastImage resizeMode={'contain'} style={styles.starImg} source={starImg} />
+            </Text>
+            <View style={{...styles.layout, justifyContent: 'space-between'}}>
+              <TouchableOpacity style={{...styles.box1, width: '49%', justifyContent: 'space-between'}}>
+                <Text style={styles.boxText}>김미경as</Text>
+                <FastImage resizeMode={'contain'} style={styles.moreImg} source={moreImg} />
+              </TouchableOpacity>
+              <TextInput style={{...styles.inputBox, width: '49%'}} placeholder={'연락처'} placeholderTextColor={mConst.borderGray} />
+            </View>
+            <View
+              style={{
+                ...styles.layout,
+                justifyContent: 'space-between',
+                paddingTop: mUtils.wScale(20),
+                paddingBottom: mUtils.wScale(18),
+              }}
+            >
+              <View style={{width: '32%'}}>
+                <Text style={styles.smallTitle}>Shooting Date</Text>
+                <TouchableOpacity style={{...styles.box1, justifyContent: 'space-between'}}>
+                  <Text style={styles.boxText}>8/4(토)</Text>
+                  <FastImage resizeMode={'contain'} style={styles.moreImg} source={moreImg} />
+                </TouchableOpacity>
+              </View>
+              <View style={{width: '32%'}}>
+                <Text style={styles.smallTitle}>Pickup Date</Text>
+                <TouchableOpacity style={{...styles.box1, justifyContent: 'space-between'}}>
+                  <Text style={styles.boxText}>8/3(금)</Text>
+                  <FastImage resizeMode={'contain'} style={styles.moreImg} source={moreImg} />
+                </TouchableOpacity>
+              </View>
+              <View style={{width: '32%'}}>
+                <Text style={styles.smallTitle}>Returning Date</Text>
+                <TouchableOpacity style={{...styles.box1, justifyContent: 'space-between'}}>
+                  <Text style={styles.boxText}>8/6 (월)</Text>
+                  <FastImage resizeMode={'contain'} style={styles.moreImg} source={moreImg} />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <Text style={styles.smallTitle}>Shipping destination</Text>
+            <View style={{...styles.box2, width: '100%'}}>
+              <Text style={styles.boxText}>강남대로 135-5295</Text>
+              <TouchableOpacity style={{...styles.postBox}}>
+                <Text style={styles.postCode}>우편번호</Text>
+              </TouchableOpacity>
+            </View>
+            <TextInput style={{...styles.inputBox, marginTop: mUtils.wScale(6), marginBottom: mUtils.wScale(18)}} />
+            <Text style={styles.smallTitle}>Shipping Notes</Text>
+            <TextInput
+              style={{...styles.inputBox, height: mUtils.wScale(75), marginTop: mUtils.wScale(6)}}
+              multiline={true}
+              textAlignVertical={'top'}
+            />
+            <View
+              style={{
+                ...styles.layout,
+                justifyContent: 'space-between',
+                paddingTop: mUtils.wScale(20),
+                paddingBottom: mUtils.wScale(18),
+              }}
+            >
+              <View style={{width: '49%'}}>
+                <Text style={styles.smallTitle}>촬영컨셉</Text>
+                <TextInput style={{...styles.inputBox}} placeholder={'컨셉'} placeholderTextColor={mConst.borderGray} />
+              </View>
+              <View style={{width: '49%'}}>
+                <Text style={styles.smallTitle}>Shooting time</Text>
+                <TextInput style={{...styles.inputBox}} placeholder={'시간'} placeholderTextColor={mConst.borderGray} />
+              </View>
+            </View>
+            <Text style={styles.smallTitle}>
+              Model <FastImage resizeMode={'contain'} style={styles.starImg} source={starImg} />
+            </Text>
+            <View style={{...styles.layout, justifyContent: 'space-between', width: '100%'}}>
+              <View style={{...styles.layout}}>
+                <FastImage resizeMode={'contain'} style={styles.checkImg} source={checkImg} />
+                <Text style={{...styles.smallTitle, marginBottom: 0, marginLeft: mUtils.wScale(5)}}>Celebrity</Text>
+              </View>
+              <View style={{...styles.box2, width: '65%'}}>
+                <Text style={styles.boxText}>아이린</Text>
+                <FastImage resizeMode={'contain'} style={styles.plusImg} source={plusImg} />
+              </View>
+            </View>
+            <View
+              style={{...styles.layout, justifyContent: 'space-between', width: '100%', marginTop: mUtils.wScale(5), marginBottom: mUtils.wScale(18)}}
+            >
+              <View style={{...styles.layout}}>
+                <FastImage resizeMode={'contain'} style={styles.checkImg} source={noCheckImg} />
+                <Text style={{...styles.smallTitle, marginBottom: 0, marginLeft: mUtils.wScale(5)}}>Fashion Model</Text>
+              </View>
+              <View style={{...styles.box2, width: '65%'}}>
+                <Text style={styles.boxText}></Text>
+                <FastImage resizeMode={'contain'} style={styles.plusImg} source={plusImg} />
+              </View>
+            </View>
+            <Text style={styles.smallTitle}>
+              유가화보 <FastImage resizeMode={'contain'} style={styles.starImg} source={starImg} />
+            </Text>
+            <View style={{...styles.layout, justifyContent: 'space-between', marginBottom: mUtils.wScale(18)}}>
+              <FastImage resizeMode={'contain'} style={styles.checkImg} source={noCheckImg} />
+              <TextInput style={{...styles.inputBox, width: '95%'}} placeholder={'Brand Name'} placeholderTextColor={mConst.borderGray} />
+            </View>
+            <Text style={styles.smallTitle}>당일연결 희망/ 가능 여부</Text>
+            <View
+              style={{...styles.layout, width: '100%', justifyContent: 'space-between', marginBottom: mUtils.wScale(18), marginTop: mUtils.wScale(3)}}
+            >
+              {yesNo.map((item, index) => {
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => {
+                      this.setState({...this.state, yesNo: item})
+                    }}
+                    style={{...styles.yesNoBox, borderColor: this.state.yesNo === item ? mConst.black : mConst.borderGray}}
+                  >
+                    <FastImage resizeMode={'contain'} style={styles.checkImg2} source={this.state.yesNo === item ? checkImg2 : checkImg3} />
+                    <Text
+                      style={{...styles.yesNo, marginLeft: mUtils.wScale(5), color: this.state.yesNo === item ? mConst.black : mConst.borderGray}}
+                    >
+                      {item}
+                    </Text>
+                  </TouchableOpacity>
+                )
+              })}
+            </View>
+            <Text style={styles.smallTitle}>Number of Pages</Text>
+            <TextInput
+              style={{...styles.inputBox, marginTop: mUtils.wScale(3), marginBottom: mUtils.wScale(18)}}
+              placeholder={'Number of pages'}
+              placeholderTextColor={'#555555'}
+            />
+            <Text style={styles.smallTitle}>함께 들어가는 브랜드</Text>
+            <TextInput
+              style={{...styles.inputBox, marginTop: mUtils.wScale(3), marginBottom: mUtils.wScale(18)}}
+              placeholder={'Different brand'}
+              placeholderTextColor={'#555555'}
+            />
+            <Text style={styles.smallTitle}>Message</Text>
+            <TextInput
+              style={{...styles.inputBox, height: mUtils.wScale(75), marginTop: mUtils.wScale(3), marginBottom: mUtils.wScale(18)}}
+              multiline={true}
+              textAlignVertical={'top'}
+            />
+          </View>
         </ScrollView>
+        <View style={styles.layout}>
+          <TouchableOpacity style={{...styles.bottomButton, backgroundColor: mConst.borderGray}}>
+            <Text style={{...styles.buttonText, color: mConst.black}}>Temporary Save</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{...styles.bottomButton, backgroundColor: mConst.black}}>
+            <Text style={{...styles.buttonText, color: mConst.white}}>Sample Request</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     )
   }
--- src/screens/HomeScreens/SampleRequestsScreen/styles.js
+++ src/screens/HomeScreens/SampleRequestsScreen/styles.js
@@ -1,3 +1,4 @@
+import {Platform} from 'react-native'
 import {StyleSheet} from 'react-native'
 
 import mConst from '../../../common/constants'
@@ -25,5 +26,127 @@ export default StyleSheet.create({
     fontSize: 18,
     textAlign: 'left',
     color: mConst.black,
+  },
+  modelTitle: {
+    fontFamily: 'Roboto-Medium',
+    fontSize: 15,
+    textAlign: 'center',
+    color: '#070708',
+  },
+  buttonText: {
+    fontFamily: 'Roboto-Bold',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  smallTitle: {
+    fontFamily: 'Roboto-Bold',
+    fontSize: 14,
+    textAlign: 'left',
+    color: mConst.black,
+    marginBottom: mUtils.wScale(8),
+  },
+  boxText: {
+    fontFamily: 'Roboto-Regular',
+    fontSize: 14,
+    textAlign: 'left',
+    color: '#555555',
+  },
+  postCode: {
+    fontFamily: 'NotoSansKR-Medium',
+    fontSize: 12,
+    textAlign: 'center',
+    color: mConst.white,
+  },
+  yesNo: {
+    fontFamily: 'Roboto-Regular',
+    fontSize: 14,
+    textAlign: 'left',
+  },
+  yesNoBox: {
+    width: '49%',
+    paddingVertical: mUtils.wScale(10),
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: mUtils.wScale(10),
+  },
+  postBox: {
+    backgroundColor: mConst.black,
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    borderColor: mConst.borderGray,
+    paddingHorizontal: mUtils.wScale(8),
+    paddingVertical: mUtils.wScale(8),
+  },
+  emptyBar: {
+    width: '100%',
+    height: mUtils.wScale(6),
+    backgroundColor: '#f6f6f6',
+    marginVertical: mUtils.wScale(30),
+  },
+  bottomButton: {
+    width: '50%',
+    paddingVertical: mUtils.wScale(18),
+  },
+  layout: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  box1: {
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    borderColor: mConst.borderGray,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: mUtils.wScale(8),
+    paddingVertical: mUtils.wScale(8),
+  },
+  box2: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    borderColor: mConst.borderGray,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: mUtils.wScale(8),
+  },
+  inputBox: {
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    borderColor: mConst.borderGray,
+    fontFamily: 'Roboto-Regular',
+    fontSize: 14,
+    textAlign: 'left',
+    color: '#555555',
+    paddingHorizontal: mUtils.wScale(8),
+    paddingVertical: Platform.OS === 'ios' ? mUtils.wScale(7) : mUtils.wScale(1.2),
+  },
+  modelImg: {
+    width: mUtils.wScale(157.5),
+    height: mUtils.wScale(236.5),
+  },
+  moreImg: {
+    width: mUtils.wScale(13),
+    height: mUtils.wScale(13),
+  },
+  starImg: {
+    width: mUtils.wScale(12),
+    height: mUtils.wScale(12),
+    marginTop: mUtils.wScale(1),
+  },
+  checkImg: {
+    width: mUtils.wScale(12),
+    height: mUtils.wScale(12),
+  },
+  plusImg: {
+    width: mUtils.wScale(35),
+    height: mUtils.wScale(35),
+  },
+  checkImg2: {
+    width: mUtils.wScale(12),
+    height: mUtils.wScale(12),
   },
 })
