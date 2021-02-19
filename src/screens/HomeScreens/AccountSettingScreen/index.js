7f1f63a26f8be888488e95af9befc1165c6bbdab
LeeKyungJoon <wns965@svcorps.dev>
2021-02-19 18:05:29 +0900

    ***props 수정 및 전체 화면 수정(기획 이슈)

--- src/screens/HomeScreens/AccountSettingScreen/index.js
+++ src/screens/HomeScreens/AccountSettingScreen/index.js
@@ -1,5 +1,5 @@
 import React, {PureComponent} from 'react'
-import {SafeAreaView, View, ScrollView, Switch, Platform, TouchableOpacity} from 'react-native'
+import {SafeAreaView, View, ScrollView} from 'react-native'
 import {connect} from 'react-redux'
 import FastImage from 'react-native-fast-image'
 import _ from 'lodash'
@@ -8,9 +8,9 @@ import mConst from '../../../common/constants'
 import mUtils from '../../../common/utils'
 import cBind, {callOnce} from '../../../common/navigation'
 import Text from '../../common/Text'
-import {Grid, Col, Row} from 'react-native-easy-grid'
 import styles from './styles'
-import {multicastChannel} from 'redux-saga'
+import {logout} from '../../../common/aws-auth'
+import {actionLogout} from '../../../redux/actions'
 
 const profileImage = require('../../../images/navi/profile_1.png')
 const goImage = require('../../../images/navi/go_1.png')
@@ -21,53 +21,63 @@ class AccountSettingScreen extends PureComponent {
     cBind(this)
   }
 
+  handleLogout = callOnce(() => {
+    const {logoutSuccess, logoutFailure} = this.props
+    logout({
+      cbSuccess: async response => {
+        logoutSuccess(response)
+        console.log('###로그아웃 성공:', response)
+      },
+      cbFailure: async e => {
+        logoutFailure(e)
+        console.log('###로그아웃 실패', e)
+      },
+    })
+  })
+
   componentDidMount() {
-    this.pushOption('계정설정')
+    this.logOutOption('계정설정', this.handleLogout)
   }
 
   render() {
+    const {info} = this.props.route.params
     return (
       <>
         <SafeAreaView style={styles.container}>
           <ScrollView contentContainerStyle={styles.scroll}>
-            <TouchableOpacity style={styles.img}>
-              <FastImage resizeMode={'contain'} style={styles.profileImg} source={profileImage} />
-            </TouchableOpacity>
+            <View style={styles.img}>
+              <FastImage resizeMode={'contain'} style={styles.profileImg} source={info.img_url_adres} />
+            </View>
             <View style={styles.top}>
               <Text style={styles.title}>이름</Text>
-              <TouchableOpacity style={styles.box}>
-                <Text style={styles.desc}>최서영</Text>
-                <FastImage resizeMode={'contain'} style={styles.goImg} source={goImage} />
-              </TouchableOpacity>
+              <View style={styles.box}>
+                <Text style={styles.desc}>{info.brand_user_nm}</Text>
+              </View>
             </View>
             <View style={styles.top}>
               <Text style={styles.title}>직급</Text>
-              <TouchableOpacity style={styles.box}>
-                <Text style={styles.desc}>인턴</Text>
-                <FastImage resizeMode={'contain'} style={styles.goImg} source={goImage} />
-              </TouchableOpacity>
+              <View style={styles.box}>
+                <Text style={styles.desc}>{info.user_position}</Text>
+              </View>
             </View>
             <View style={styles.top}>
               <Text style={styles.title}>이메일</Text>
-              <TouchableOpacity style={styles.box}>
-                <Text style={styles.desc}>asd123@naver.com</Text>
-                <FastImage resizeMode={'contain'} style={styles.goImg} source={goImage} />
-              </TouchableOpacity>
+              <View style={styles.box}>
+                <Text style={styles.desc}>{info.email_adres}</Text>
+              </View>
             </View>
             <View style={styles.top}>
               <Text style={styles.title}>휴대폰 번호</Text>
-              <TouchableOpacity style={styles.box}>
-                <Text style={styles.desc}>010-1111-2222</Text>
-                <FastImage resizeMode={'contain'} style={styles.goImg} source={goImage} />
-              </TouchableOpacity>
+              <View style={styles.box}>
+                <Text style={styles.desc}>
+                  {info.phone_no
+                    .replace(/[^0-9]/g, '')
+                    .replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/, '$1-$2-$3')
+                    .replace('--', '-')}
+                </Text>
+              </View>
             </View>
-            <View style={styles.top}>
-              <Text style={styles.title}>비밀번호</Text>
-              <TouchableOpacity style={styles.box}>
-                <Text style={{...styles.desc, fontSize: 12}}>● ● ● ● ● ● ● ●</Text>
-                <FastImage resizeMode={'contain'} style={styles.goImg} source={goImage} />
-              </TouchableOpacity>
-            </View>
+            <Text style={{...styles.alert}}>* 프로필수정은 PC에서만 가능합니다.</Text>
           </ScrollView>
         </SafeAreaView>
       </>
@@ -77,5 +87,8 @@ class AccountSettingScreen extends PureComponent {
 
 export default connect(
   state => ({}),
-  dispatch => ({})
+  dispatch => ({
+    logoutSuccess: data => dispatch(actionLogout.success(data)),
+    logoutFailure: data => dispatch(actionLogout.failure(data)),
+  })
 )(AccountSettingScreen)
