180c94660000c7f40ebe3990e888b731869ddc3a
LeeKyungJoon <wns965@svcorps.dev>
2021-02-23 18:08:10 +0900

    ***화면 수정(기획 이슈)

--- src/screens/HomeScreens/MyPageScreen/index.js
+++ src/screens/HomeScreens/MyPageScreen/index.js
@@ -62,23 +62,23 @@ class MyPageScreen extends PureComponent {
             >
               <Text style={styles.text1}>문의하기</Text>
             </TouchableOpacity>
-            <Text style={styles.text1}>설정</Text>
             <TouchableOpacity
-              style={{paddingTop: mUtils.wScale(40)}}
               onPress={() => {
                 navigation.navigate('NotificationScreen')
               }}
             >
-              <Text style={styles.text2}>알림</Text>
+              <Text style={styles.text1}>알림</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={{paddingTop: mUtils.wScale(40)}}>
-              <Text style={styles.text2}>공지사항</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{paddingTop: mUtils.wScale(40)}}>
-              <Text style={styles.text2}>안내사항</Text>
+            <TouchableOpacity
+              style={{paddingTop: mUtils.wScale(40)}}
+              onPress={() => {
+                navigation.navigate('NoticeListScreen')
+              }}
+            >
+              <Text style={styles.text1}>공지사항</Text>
             </TouchableOpacity>
             <View style={styles.version}>
-              <Text style={styles.text2}>버전 정보</Text>
+              <Text style={styles.text1}>버전 정보</Text>
               <Text style={styles.text2}>{DeviceInfo.getVersion()}</Text>
             </View>
           </ScrollView>
