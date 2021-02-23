e37970d99ea56d576a0776d0ce1216d111850cb0
LeeKyungJoon <wns965@svcorps.dev>
2021-02-23 18:08:29 +0900

    ***화면 추가

---
+++ src/screens/HomeScreens/NoticeListScreen/index.js
@@ -0,0 +1,56 @@
+import React, {PureComponent} from 'react'
+import {SafeAreaView, FlatList, View, TouchableOpacity} from 'react-native'
+import {connect} from 'react-redux'
+import FastImage from 'react-native-fast-image'
+import _ from 'lodash'
+import moment from 'moment'
+
+import mConst from '../../../common/constants'
+import mUtils from '../../../common/utils'
+import cBind, {callOnce} from '../../../common/navigation'
+import Text from '../../common/Text'
+import styles from './styles'
+import API from '../../../common/aws-api'
+import Loading from '../../common/Loading'
+
+class NoticeListScreen extends PureComponent {
+  constructor(props) {
+    super(props)
+    cBind(this)
+    this.state = {list: []}
+  }
+
+  renderItem = ({item}) => {
+    return (
+      <TouchableOpacity style={styles.itemBox} onPress={() => {}}>
+        <Text style={styles.title}>{item.inqry_subj}</Text>
+        <Text style={styles.dt}>작성일: {moment(item.inqry_dt * 1000).format('YYYY.MM.DD')}</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  componentDidMount() {
+    this.pushOption('공지사항')
+  }
+
+  render() {
+    const {list} = this.state
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          style={styles.list}
+          data={list}
+          renderItem={this.renderItem}
+          keyExtractor={item => item.inqry_dt}
+          //onEndReached={this.handleLoadMore}
+          //onEndReachedThreshold={1}
+        />
+      </SafeAreaView>
+    )
+  }
+}
+
+export default connect(
+  state => ({}),
+  dispatch => ({})
+)(NoticeListScreen)
---
+++ src/screens/HomeScreens/NoticeListScreen/styles.js
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native'
+
+import mConst from '../../../common/constants'
+import mUtils from '../../../common/utils'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: mConst.white,
+  },
+  itemBox: {
+    paddingVertical: mUtils.wScale(20),
+    width: '100%',
+    borderStyle: 'solid',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ebebeb',
+  },
+  title: {
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: 16,
+    fontStyle: 'normal',
+    textAlign: 'left',
+    color: mConst.black,
+  },
+  dt: {
+    fontFamily: 'NotoSansKR-Regular',
+    fontSize: 14,
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    textAlign: 'left',
+    color: mConst.gray,
+    marginTop: mUtils.wScale(3.5),
+  },
+  list: {
+    paddingHorizontal: mUtils.wScale(20),
+  },
+})
