11ad7f63821405693ea362aeb19014e868474325
최광석 <compert@svcorps.dev>
2021-07-02 10:29:40 +0900

    필터 항목 일괄 정리

--- src/screens/HomeScreens/FilterScreen/index.js
+++ src/screens/HomeScreens/FilterScreen/index.js
@@ -16,13 +16,14 @@ import BrandGroup from '../../common/BrandGroup'
 import styles from './styles'
 import API from '../../../common/aws-api'
 
-const genders = ['여성', '남성', '유니섹스']
-const sections = ['Brands', 'Category', 'Availability', 'Color', 'Size', 'Sample', 'Still Life Image', 'Material']
+const genders = ['Women', 'Men', 'Unisex']
+const sections = ['Brand', 'Category', 'Color', 'Size', 'Sample', 'Still Life Image', 'Material']
 const categories = [
-  ['Jewelry', '링', '브레이슬릿', '이어링', '네크리스', '워치', 'Others'],
+  ['RTW', '아우터웨어', '탑 & 셔츠', '드레스', '팬츠', '스커트', '티셔츠 & 스웨트셔츠', '니트웨어', '언더웨어 & 스윔웨어', '액티브 웨어', '데님'],
+  ['Bag', '탑 핸들 백', '토트 백', '숄더 백', '크로스바디 백', '벨트 백 & 백팩', '미니 백', '클러치 & 포트폴리오', '러기지 & 트레블 백'],
+  ['Deco', '캔들', '체어', '월페이퍼', '스크린', '트레이', '테이블웨어', '쿠션', '담요', '머그'],
   ['Accessory', '벨트', '아이웨어', '넥타이', '모자', '스카프 & 장갑', '테크', '지갑', '파우치', 'Others'],
-  ['Handbag', '탑 핸들 백', '토트 백', '숄더 백', '크로스바디 백', '벨트 백 & 백팩', '미니 백', '클러치 & 포트폴리오', '러기지 & 트레블 백'],
-  ['Decor', '캔들', '체어', '월페이퍼', '스크린', '트레이', '테이블웨어', '쿠션', '담요', '머그'],
+  ['Jewelry', '링', '브레이슬릿', '이어링', '네크리스', '워치', 'Others'],
   [
     'Shoes',
     '스니커즈',
@@ -36,7 +37,6 @@ const categories = [
     '에스파드리유 & 웨지',
     '레이스업',
   ],
-  ['RTW', '아우터웨어', '탑 & 셔츠', '드레스', '팬츠', '스커트', '티셔츠 & 스웨트셔츠', '니트웨어', '언더웨어 & 스윔웨어', '액티브 웨어', '데님'],
 ]
 const colors = [
   ['white', '화이트'],
@@ -53,8 +53,22 @@ const colors = [
   ['pink', '핑크'],
   ['multi', '멀티컬러'],
 ]
-const materials = ['카프스킨', '램 스킨', '코든', '스웨이드', '벨벳', '데님', '린넨', '쉬폰', '가죽', '퍼', 'Others']
-//const brands = ['A BATHING APE', 'A COLD WALL', 'A.BELL', 'A FRANCE', 'A.L.C', 'BATHING APE', 'CHANEL', 'GUCCI', 'LOUISVUITTON']
+const materials = [
+  '코튼',
+  '레더',
+  '레더-카프스킨',
+  '레더-램스킨',
+  '레더-프리셔스 스킨',
+  '울',
+  '캔버스',
+  '데님',
+  '벨벳',
+  '실크',
+  '시어링',
+  '지속가능한 소재',
+  'etc',
+]
+
 const closeBtnImage = require('../../../images/navi/close.png')
 const foldImage = require('../../../images/common/fold.png')
 const unfoldImage = require('../../../images/common/unfold.png')
@@ -69,7 +83,6 @@ class FilterScreen extends PureComponent {
     this.state = {
       gender: genders[0],
       section: mConst.getUserType() === 'M' ? sections[0] : sections[1],
-      brandDrop: false,
       brands: [],
       search: [],
       selectBrand: '',
@@ -153,7 +166,7 @@ class FilterScreen extends PureComponent {
               </Row>
               <Row>
                 <Col size={35}>
-                  {_.map(userType === 'M' ? sections : sections.filter(e => e !== 'Brands'), (item, index) => {
+                  {_.map(userType === 'M' ? sections : sections.filter(e => e !== 'Brand'), (item, index) => {
                     const selected = item === section
                     return (
                       <TouchableOpacity key={index} onPress={() => this.handleSelectSection(item)}>
@@ -165,28 +178,15 @@ class FilterScreen extends PureComponent {
                   })}
                 </Col>
                 <Col size={65}>
-                  {section === 'Category' && _.map(categories, (item, index) => <CategoryGroup key={index} data={item} />)}
-                  {section === 'Color' && <ColorGroup data={colors} />}
-                  {section === 'Material' && <MaterialGroup data={materials} />}
-                  {section === 'Brands' && (
+                  {section === 'Brand' && (
                     <Col>
-                      <TouchableOpacity
-                        style={styles.layout}
-                        onPress={() => {
-                          this.setState({brandDrop: !brandDrop})
-                        }}
-                      >
-                        <Text style={styles.brandText}>Brand</Text>
-                        {brandDrop ? (
-                          <FastImage resizeMode={'contain'} source={moreImgOn} style={styles.moreImg} />
-                        ) : (
-                          <FastImage resizeMode={'contain'} source={moreImg} style={styles.moreImg} />
-                        )}
-                      </TouchableOpacity>
-                      {brandDrop && <BrandGroup data={brands} search={this.searchResult} brand={this.selectBrand} />}
+                      <BrandGroup data={brands} search={this.searchResult} brand={this.selectBrand} />
                     </Col>
                   )}
                   {['Availability', 'Size', 'Sample', 'Still Life Image'].includes(section) && <Text>"{section}" : 서비스 준비중입니다.</Text>}
+                  {section === 'Category' && _.map(categories, (item, index) => <CategoryGroup key={index} data={item} />)}
+                  {section === 'Color' && <ColorGroup data={colors} />}
+                  {section === 'Material' && <MaterialGroup data={materials} />}
                 </Col>
               </Row>
             </Grid>
